1a564ff19fad9de131689ab8d25ef99d00146b43
mariaxboza <65983691+mariaxboza@users.noreply.github.com>
2020-05-26 18:57:43 -0400

    Update ImageList.java

--- ImageList.java
+++ ImageList.java
@@ -3,6 +3,7 @@ import MemoryGame.Pictures.Empty;
 import javax.swing.*;
 
 public class ImageList {
+    // Class to create the pictures for the game
     private static ImageIcon pic1 = new ImageIcon(Empty.class.getResource("pic1.png"));
     private static ImageIcon pic2 = new ImageIcon(Empty.class.getResource("pic2.png"));
     private static ImageIcon pic3 = new ImageIcon(Empty.class.getResource("pic3.png"));
@@ -12,12 +13,13 @@ public class ImageList {
     private static ImageIcon pic7 = new ImageIcon(Empty.class.getResource("pic7.png"));
     private static ImageIcon pic8 = new ImageIcon(Empty.class.getResource("pic8.png"));
 
+    // Array for all the pictures in the game
     private ImageIcon[] pics = {pic1, pic2, pic3, pic4 , pic5, pic6, pic7, pic8 , pic1, pic2,
                                 pic3, pic4 , pic5, pic6, pic7, pic8};
 
 
 
-    public ImageIcon[][] getPics() {
+    public ImageIcon[][] getPics() {   // Method to get the picture array 
         ImageIcon[][] pic = new ImageIcon[4][4];
         int x = (int)(Math.random() * 4) + 2;
 
