6b3b6762a48d613a735a151ea2311c92f331c96d
mariaxboza <65983691+mariaxboza@users.noreply.github.com>
2020-05-26 19:04:08 -0400

    Update gameButtons.java

--- gameButtons.java
+++ gameButtons.java
@@ -1,17 +1,17 @@
 import javax.swing.*;
 
-public class gameButtons extends JButton{
+public class gameButtons extends JButton{ // Class to create the game buttons 
     private ImageIcon _i;
 
     public gameButtons(ImageIcon i)
-    {
+    {   // Constructor which will be used to create the button with the pictures
         super();
         _i = i;
 
     }
 
     public gameButtons()
-    {
+    {   // Constuctor which will be used to create the blank button 
         super();
     }
 
